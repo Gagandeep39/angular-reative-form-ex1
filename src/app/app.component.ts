a8ca56d5e46275f7da7e21660bacbfdc7a404cd6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-18 09:43:34 +0530

    Created a FormGroup for name and email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'secret': new FormControl(null, Validators.required),
       'gender': new FormControl(null, Validators.required),
       'cities': new FormArray([], Validators.required),
